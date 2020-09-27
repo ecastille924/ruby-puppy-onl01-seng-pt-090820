662bfe5cede1324c708c7a129c81801595173ab3
Erick Castille <ecastille924@gmail.com>
2020-09-27 18:24:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Add your code here
+class Dog 
+  
+  @@all = [ ]
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all 
+    @@all.each do |dog|
+      
+  end
+  
+end
